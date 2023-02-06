5025afcb1be7108e10d7bc71a805d562470f0964
Nicholas Ndure Maina <ndurenicholas@gmail.com>
2023-02-05 23:56:26 -0800

    update 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,4 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0]
-if match = input_string.match(/from:(.*?)\s.*to:(.*?)\s.*flags:(.*?)\s/)
-  puts "#{match[1]},#{match[2]},#{match[3]}"
-else
-  puts ""
-end
+# A regular expression that is matches a given pattern
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
 
